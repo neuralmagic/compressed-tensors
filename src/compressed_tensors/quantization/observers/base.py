4733283fdbaada4ceb62c7143d56bd313d655022
Benjamin Fineran <bfineran@users.noreply.github.com>
2024-04-25 10:40:51 -0400

    Apply suggestions from code review

--- src/compressed_tensors/quantization/observers/base.py
+++ src/compressed_tensors/quantization/observers/base.py
@@ -30,9 +30,6 @@ class Observer(Module, RegistryMixin):
     pair
     """
 
-    # child classes should set to True if they are meant to be used as dynamic
-    DYNAMIC = False
-
     def __init__(self, quantization_args: QuantizationArgs):
         self.quantization_args: QuantizationArgs = quantization_args
         super().__init__()
