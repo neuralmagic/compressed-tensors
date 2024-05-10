397989cf417fca53831f007487c427f650730649
Sara Adkins <sara@neuralmagic.com>
2024-05-10 21:15:34 +0000

    cleanup

--- tests/test_quantization/test_configs/test_strategies.py
+++ tests/test_quantization/test_configs/test_strategies.py
@@ -22,6 +22,7 @@ from compressed_tensors.quantization import (
     QuantizationStrategy,
     apply_quantization_config,
 )
+from compressed_tensors.quantization.lifecycle.forward import fake_quantize
 from torch.nn import Linear
 
 
