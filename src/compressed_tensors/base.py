a9af5a86b7ac55f270b3ee3a0d186fc2bb016fbe
dbogunowicz <97082108+dbogunowicz@users.noreply.github.com>
2024-06-20 13:49:02 +0200

    Update src/compressed_tensors/base.py

--- src/compressed_tensors/base.py
+++ src/compressed_tensors/base.py
@@ -15,4 +15,4 @@
 SPARSITY_CONFIG_NAME = "sparsity_config"
 QUANTIZATION_CONFIG_NAME = "quantization_config"
 COMPRESSION_CONFIG_NAME = "compression_config"
-KV_CACHE_SCHEME = "kv_cache_scheme"
+KV_CACHE_SCHEME_NAME = "kv_cache_scheme"
