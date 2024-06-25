04f68eeac14b14c8b45a5c7e5481483fa611ad34
bogunowicz@arrival.com <bogunowicz@arrival.com>
2024-06-25 13:25:47 +0000

    initial commit

--- tests/test_quantization/test_quant_config.py
+++ tests/test_quantization/test_quant_config.py
@@ -12,51 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import pytest
-from compressed_tensors.quantization import (
-    DEFAULT_QUANTIZATION_FORMAT,
-    DEFAULT_QUANTIZATION_METHOD,
-    QuantizationConfig,
-    QuantizationScheme,
-    QuantizationStatus,
+from compressed_tensors.quantization import QuantizationConfig, QuantizationScheme
+
+
+@pytest.mark.parametrize(
+    "scheme_name",
+    [
+        "W8A8",
+        "W4A16",
+    ],
 )
-from pydantic import ValidationError
+def test_load_scheme_from_preset(scheme_name: str):
+    targets = ["Linear"]
+    config = QuantizationConfig(config_groups={scheme_name: targets})
 
-
-def test_basic_config():
-    config_groups = {"group_1": QuantizationScheme(targets=[])}
-    config = QuantizationConfig(config_groups=config_groups)
-
-    assert config.config_groups == config_groups
-    assert config.quant_method == DEFAULT_QUANTIZATION_METHOD
-    assert config.format == DEFAULT_QUANTIZATION_FORMAT
-    assert config.quantization_status == QuantizationStatus.INITIALIZED
-    assert config.global_compression_ratio is None
-    assert isinstance(config.ignore, list) and len(config.ignore) == 0
-
-
-def test_full_config():
-    config_groups = {
-        "group_1": QuantizationScheme(targets=[]),
-        "group_2": QuantizationScheme(targets=[]),
-    }
-    global_compression_ratio = 3.5
-    ignore = ["model.layers.0"]
-    quantization_status = "compressed"
-
-    config = QuantizationConfig(
-        config_groups=config_groups,
-        global_compression_ratio=global_compression_ratio,
-        ignore=ignore,
-        quantization_status=quantization_status,
-    )
-    assert config.config_groups == config_groups
-    assert config.global_compression_ratio == global_compression_ratio
-    assert config.ignore == ignore
-    assert config.quantization_status == QuantizationStatus.COMPRESSED
-
-
-def test_need_config_groups():
-    with pytest.raises(ValidationError):
-        _ = QuantizationScheme()
+    assert scheme_name in config.config_groups
+    assert isinstance(config.config_groups[scheme_name], QuantizationScheme)
+    assert config.config_groups[scheme_name].targets == targets
