6b13bd208a447099ffff422913ba9039578aaf50
George Ohashi <george@neuralmagic.com>
2024-07-25 17:03:39 +0000

    fix on load

--- src/compressed_tensors/quantization/lifecycle/apply.py
+++ src/compressed_tensors/quantization/lifecycle/apply.py
@@ -279,18 +279,22 @@ def _load_quant_args_from_state_dict(
     """
     scale_name = f"{base_name}_scale"
     zp_name = f"{base_name}_zero_point"
+    g_idx_name = f"{base_name}_g_idx"
 
     state_dict_scale = state_dict.get(f"{module_name}.{scale_name}", None)
     state_dict_zp = state_dict.get(f"{module_name}.{zp_name}", None)
+    state_dict_g_idx = state_dict.get(f"{module_name}.{g_idx_name}", None)
 
     if state_dict_scale is not None:
-        # module is quantized
-        update_parameter_data(module, state_dict_scale, scale_name)
+
         if state_dict_zp is None:
             # fill in zero point for symmetric quantization
             state_dict_zp = torch.zeros_like(state_dict_scale, device="cpu")
-        update_parameter_data(module, state_dict_zp, zp_name)
 
+        # no need to initialize g_idx
+        update_parameter_data(module, state_dict_scale, scale_name)
+        update_parameter_data(module, state_dict_zp, zp_name)
+        update_parameter_data(module, state_dict_g_idx, g_idx_name)
 
 def _scheme_from_targets(
     target_to_scheme: OrderedDictType[str, QuantizationScheme],
