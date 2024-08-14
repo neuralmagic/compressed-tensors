f864eb0080413d1754011538527a1a463569378c
Sara Adkins <sara@neuralmagic.com>
2024-08-14 18:50:14 +0000

    make accelerate dependency optional

--- src/compressed_tensors/quantization/lifecycle/initialize.py
+++ src/compressed_tensors/quantization/lifecycle/initialize.py
@@ -17,8 +17,6 @@ import logging
 from typing import Optional
 
 import torch
-from accelerate.hooks import add_hook_to_module, remove_hook_from_module
-from accelerate.utils import PrefixedDataset
 from compressed_tensors.quantization.lifecycle.forward import (
     wrap_module_forward_quantized,
 )
@@ -86,6 +84,9 @@ def initialize_module_for_quantization(
 
     offloaded = False
     if is_module_offloaded(module):
+        from accelerate.hooks import add_hook_to_module, remove_hook_from_module
+        from accelerate.utils import PrefixedDataset
+        
         offloaded = True
         hook = module._hf_hook
         prefix_dict = module._hf_hook.weights_map
