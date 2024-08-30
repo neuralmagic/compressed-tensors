d734f65a68bcad7455b9ff47db2556e849ce17bb
Kyle Sayers <kylesayrs@gmail.com>
2024-08-30 17:56:40 +0000

    Only initialize g_idx if activation order is present

--- src/compressed_tensors/quantization/lifecycle/initialize.py
+++ src/compressed_tensors/quantization/lifecycle/initialize.py
@@ -21,6 +21,7 @@ from compressed_tensors.quantization.lifecycle.forward import (
     wrap_module_forward_quantized,
 )
 from compressed_tensors.quantization.quant_args import (
+    ActivationOrderingStrategy,
     QuantizationArgs,
     QuantizationStrategy,
 )
@@ -180,7 +181,7 @@ def _initialize_scale_zero_point_observer(
         module.register_parameter(f"{base_name}_zero_point", init_zero_point)
 
     # initialize with empty for actorder, to be populated by GPTQ or state_dict
-    if quantization_args.actorder:
+    if quantization_args.actorder != ActivationOrderingStrategy.OFF:
         g_idx_shape = (weight_shape[1],)
         g_idx_dtype = torch.int
         init_g_idx = Parameter(
