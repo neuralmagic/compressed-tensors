4cf85086e2dd6110e9569be8f4f00718a44fd4e8
Dipika <dipikasikka1@gmail.com>
2024-10-10 20:23:17 +0000

    update

--- src/compressed_tensors/quantization/quant_args.py
+++ src/compressed_tensors/quantization/quant_args.py
@@ -121,8 +121,9 @@ class QuantizationArgs(BaseModel, use_enum_values=True):
             self.observer = None
             return self.observer
 
-        return Observer.load_from_registry(self.observer, quantization_args=self)
+        return self.observer
 
+    # TODO: update to be removed into llm-compressor
     def get_kv_cache(self):
         """Get the singleton KV Cache"""
         from compressed_tensors.quantization.cache import QuantizedKVParameterCache
