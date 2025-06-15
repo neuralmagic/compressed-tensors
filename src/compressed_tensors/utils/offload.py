114446c3b735567d699f1ff31d713f0dbab62a47
Kyle Sayers <kylesayrs@gmail.com>
2025-06-15 00:58:52 -0400

    ensure offload device is cpu for now
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/compressed_tensors/utils/offload.py
+++ src/compressed_tensors/utils/offload.py
@@ -499,8 +499,8 @@ def offloaded_dispatch(
     :param offload_device: device that module parameters will offload to
     :return: module with offloading device hooks
     """
-    if offload_device == "disk":
-        raise NotImplementedError("Disk offloading is not currently supported")
+    if offload_device != torch.device("cpu"):
+        raise NotImplementedError("Only CPU offloading is supported")
 
     # create weights map
     state_dict = module.state_dict()
