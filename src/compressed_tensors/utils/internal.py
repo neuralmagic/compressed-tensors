44b678aee71964e94cd0e890a9195c307d64343e
Kyle Sayers <kylesayrs@gmail.com>
2025-07-07 12:29:48 -0400

    add docstring
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/compressed_tensors/utils/internal.py
+++ src/compressed_tensors/utils/internal.py
@@ -22,5 +22,8 @@ class InternalModule(torch.nn.Module):
     """
     Abstract base class for modules which are not a part of the the model definition.
     `torch.nn.Module`s which inherit from this class will not be targeted by configs
+
+    This is typically used to skip apply configs to `Observers` and `Transforms`
     """
+
     pass
