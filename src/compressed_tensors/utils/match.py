0eeab2d3e208c93fbdb565d91a484c6f066169f8
Kyle Sayers <kylesayrs@gmail.com>
2025-07-12 14:27:20 -0400

    match_named_parameters
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/compressed_tensors/utils/match.py
+++ src/compressed_tensors/utils/match.py
@@ -23,7 +23,13 @@ import torch
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
-__all__ = ["match_named_modules", "is_match", "match_name", "match_class"]
+__all__ = [
+    "match_named_modules",
+    "match_named_parameters",
+    "is_match",
+    "match_name",
+    "match_class",
+]
 
 
 def match_named_modules(
@@ -40,15 +46,50 @@ def match_named_modules(
     :param targets: target strings, potentially containing "re:" prefixes
     :param ignore: targets to ignore, potentially containing "re:" prefixes
     :param warn_on_fail: if True, warns if any targets do not match any modules in model
+    :return: generator of module names and modules
     """
     unmatched_targets = set(targets)
     for name, module in model.named_modules():
         for target in targets:
             if is_match(name, module, target):
-                unmatched_targets.remove(target)
+                unmatched_targets -= target
 
                 if not any(is_match(name, module, ign) for ign in ignore):
                     yield name, module
+
+    if warn_on_fail:
+        for target in unmatched_targets:
+            _LOGGER.warning(
+                f"Could not match `{target}` in instance of {model.__class__.__name__}"
+            )
+
+
+def match_named_parameters(
+    model: torch.nn.Module,
+    targets: Iterable[str],
+    ignore: Iterable[str],
+    warn_on_fail: bool = False,
+) -> Generator[Tuple[str, torch.nn.Module, torch.nn.Parameter]]:
+    """
+    Yields parameters which match `targets` but do not match `ignore`.
+    Values are returned in order of `model.named_modules()`
+
+    :param model: model containing params to match against
+    :param targets: target strings, potentially containing "re:" prefixes
+    :param ignore: targets to ignore, potentially containing "re:" prefixes
+    :param warn_on_fail: if True, warns if any targets do not match any params in model
+    :return: generator of fully-qualified param names, parent modules, and params
+    """
+    unmatched_targets = set(targets)
+    for module_name, module in model.named_modules():
+        for param_name, param in module.named_parameters(recurse=False):
+            param_fqn = f"{module_name}.{param_name}"
+            for target in targets:
+                if match_name(param_fqn, target):
+                    unmatched_targets -= target
+
+                    if not any(match_name(param_fqn, ign) for ign in ignore):
+                        yield param_fqn, module, param
 
     if warn_on_fail:
         for target in unmatched_targets:
