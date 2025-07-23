a097159e1332fb445ed1925f3094b228392331e3
Kyle Sayers <kylesayrs@gmail.com>
2025-07-23 16:28:58 -0400

    wip
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- tests/test_transform/factory/test_correctness.py
+++ tests/test_transform/factory/test_correctness.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from compressed_tensors.quantization import QuantizationArgs, QuantizationScheme, QuantizationConfig, apply_quantization_config, QuantizationStatus, QuantizationType, QuantizationStrategy
 import pytest
 import torch
 from compressed_tensors.transform import (
@@ -24,6 +25,7 @@ from compressed_tensors.transform import (
 from compressed_tensors.utils import offloaded_dispatch
 from tests.test_transform.conftest import MockAttention
 from tests.testing_utils import requires_accelerate, requires_gpu
+from collections import OrderedDict
 
 
 @pytest.mark.parametrize("type", ("hadamard", "random-hadamard"))
@@ -203,3 +205,122 @@ def test_correctness_attention_heads(type, randomized, head_dim, input_batch_siz
 
     output = attention(input)
     assert torch.allclose(true_output, output, atol=1e-5, rtol=0.0)
+
+
+def create_model(sizes, state_dict = None):
+    model = torch.nn.Sequential(OrderedDict([
+        ("A", torch.nn.Linear(sizes[0], sizes[1], bias=False)),
+        ("B", torch.nn.Linear(sizes[1], sizes[2], bias=False)),
+    ])).eval()
+
+    if state_dict is not None:
+        model.load_state_dict(state_dict)
+
+    return model
+
+
+@pytest.mark.parametrize("sizes", [
+    (128, 128, 128),
+    (256, 256, 256),
+    (512, 512, 512),
+    (4096, 4096, 4096),
+])
+@pytest.mark.parametrize("type", ("hadamard", "random-hadamard"))
+@pytest.mark.parametrize("quant_strategy", ("group", "tensor"))
+#@pytest.mark.parametrize("randomize", (True, False))
+#@pytest.mark.parametrize("head_dim", (4, 8))
+def test_quantization_reconstruction(
+    sizes,
+    type,
+    #randomize,
+    #head_dim,
+    quant_strategy,
+    mock_per_group_calibration,
+    mock_per_tensor_calibration,
+):
+    model_a = create_model(sizes)
+    model_b = create_model(sizes, model_a.state_dict())
+
+    transform_config = TransformConfig(
+        config_groups={
+            "": TransformScheme(
+                type=type,
+                #randomize=randomize,
+                #head_dim=head_dim,
+                apply=[
+                    TransformArgs(targets="A", location="weight_output"),
+                    TransformArgs(targets="B", location="weight_input", inverse=True),
+                ],
+            )
+        }
+    )
+
+    if quant_strategy == "group":
+        quant_config = QuantizationConfig(
+            config_groups={
+                "group_1": QuantizationScheme(
+                    targets=["Linear"],
+                    weights=QuantizationArgs(
+                        num_bits=4,
+                        type=QuantizationType.INT,
+                        strategy=QuantizationStrategy.GROUP,
+                        group_size=128,
+                        symmetric=True,
+                        dynamic=False,
+                    )
+                )
+            }, quantization_status=QuantizationStatus.CALIBRATION
+        )
+    elif quant_strategy == "tensor":
+        quant_config = QuantizationConfig(
+            config_groups={
+                "group_1": QuantizationScheme(
+                    targets=["Linear"],
+                    weights=QuantizationArgs(
+                        num_bits=4,
+                        type=QuantizationType.INT,
+                        strategy=QuantizationStrategy.TENSOR,
+                        symmetric=True,
+                        dynamic=False,
+                    )
+                )
+            }, quantization_status=QuantizationStatus.CALIBRATION
+        )
+
+
+    # full precision
+    input = torch.eye(sizes[0], requires_grad=False)
+    output_full = model_a(input)
+
+    # quant
+    apply_quantization_config(model_a, quant_config)
+    if quant_strategy == "group":
+        mock_per_group_calibration(model_a.A, base_name="weight", value=model_a.A.weight, group_size=128)
+        mock_per_group_calibration(model_a.B, base_name="weight", value=model_a.B.weight, group_size=128)
+    elif quant_strategy == "tensor":
+        mock_per_tensor_calibration(model_a.A, "weight", value=model_a.A.weight)
+        mock_per_tensor_calibration(model_a.B, "weight", value=model_a.B.weight)
+
+    output_quant = model_a(input)
+
+    # transform + quant
+    apply_transform_config(model_b, transform_config)
+    apply_quantization_config(model_b, quant_config)
+    if quant_strategy == "group":
+        mock_per_group_calibration(model_a.A, base_name="weight", value=model_a.A.weight, group_size=128)
+        mock_per_group_calibration(model_a.B, base_name="weight", value=model_a.B.weight, group_size=128)
+    elif quant_strategy == "tensor":
+        mock_per_tensor_calibration(model_a.A, "weight", value=model_a.A.weight)
+        mock_per_tensor_calibration(model_a.B, "weight", value=model_a.B.weight)
+
+    output_trans_quant = model_b(input)
+
+    loss = torch.nn.MSELoss()
+    quant_loss = loss(output_quant, output_full)
+    trans_quant_loss = loss(output_trans_quant, output_full)
+
+    threshold = 1e-05 if quant_strategy == "group" else 1e-03
+    #assert quant_loss < threshold
+    #assert trans_quant_loss < threshold
+    assert trans_quant_loss < quant_loss
+    # assert trans_quant_loss < quant_loss < 1e-05
